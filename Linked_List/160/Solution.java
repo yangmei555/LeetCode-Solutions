a34cbff253d28fc15895d539866973c89765bcbe
yangmei555 <myang3@uchicago.edu>
2018-04-01 23:38:12 -0500

    add another solution

--- Linked_List/160/Solution.java
+++ Linked_List/160/Solution.java
@@ -44,3 +44,26 @@ public class Solution {
         return null;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode nodeA = headA, nodeB = headB;
+        while (nodeA != nodeB) {
+            nodeA = nodeA == null ? headB : nodeA.next;
+            nodeB = nodeB == null ? headA : nodeB.next;
+        }
+        return nodeA;
+    }
+}
