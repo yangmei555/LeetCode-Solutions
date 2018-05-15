cffe13d901360aa0a35c9d05a3e763a606370099
yangmei555 <myang3@uchicago.edu>
2018-05-15 01:06:42 -0500

    add some solutions

--- String/165/Solution.java
+++ String/165/Solution.java
@@ -106,3 +106,44 @@ class Solution {
         }
     }
 }
+
+
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int len = v1.length > v2.length ? v1.length : v2.length;
+        for (int i = 0; i < len; i++) {
+            int n1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int n2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+            if (n1 != n2)
+                return n1 > n2 ? 1 : -1;
+        }
+        return 0;
+    }
+}
+
+
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        char[] v1 = version1.toCharArray();
+        char[] v2 = version2.toCharArray();
+        int i = 0, j = 0;
+        while (i < v1.length || j < v2.length) {
+            int n1 = 0, n2 = 0;
+            while (i < v1.length && v1[i] != '.') {
+                n1 = n1 * 10 + v1[i] - '0';
+                i++;
+            }
+            while (j < v2.length && v2[j] != '.') {
+                n2 = n2 * 10 + v2[j] - '0';
+                j++;
+            }
+            if (n1 != n2)
+                return n1 > n2 ? 1 : -1;
+            i++;
+            j++;
+        }
+        return 0;
+    }
+}
