10eb786c5d2e17135d284c1ce9add227c7b9d7dc
yangmei555 <myang3@uchicago.edu>
2018-06-07 01:15:35 -0500

    add some solutions

--- Depth_First_Search/491/Solution.java
+++ Depth_First_Search/491/Solution.java
@@ -112,3 +112,28 @@ class Solution {
         }
     }
 }
+
+
+class Solution {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        List<Integer> temp = new ArrayList<>();
+        helper(nums, res, temp, 0);
+        return res;
+    }
+    
+    public void helper(int[] nums, List<List<Integer>> res, List<Integer> temp, int index) {
+        if (index == nums.length) {
+            if (temp.size() > 1)
+                res.add(new ArrayList<>(temp));
+            return;
+        }
+        if (temp.size() == 0 || temp.get(temp.size()-1) != nums[index])
+            helper(nums, res, temp, index+1);
+        if (temp.size() == 0 || temp.get(temp.size()-1) <= nums[index]) {
+            temp.add(nums[index]);
+            helper(nums, res, temp, index+1);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
