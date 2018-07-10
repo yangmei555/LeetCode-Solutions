09190a0e37a35fcfa69bec003cb46a39aa2aaea4
yangmei555 <myang3@uchicago.edu>
2018-07-09 21:25:28 -0500

    add some solutions

--- Binary_Search_Tree/783/Solution.java
+++ Binary_Search_Tree/783/Solution.java
@@ -35,3 +35,28 @@ class Solution {
         return res;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int pre = Integer.MIN_VALUE, res = Integer.MAX_VALUE;
+    
+    public int minDiffInBST(TreeNode root) {
+        if (root == null)
+            return Integer.MAX_VALUE;
+        int left = minDiffInBST(root.left), self = Integer.MAX_VALUE;
+        if (pre != Integer.MIN_VALUE)
+            self = root.val - pre;
+        pre = root.val;
+        int right = minDiffInBST(root.right);
+        return Math.min(Math.min(left, self), right);
+    }
+}
