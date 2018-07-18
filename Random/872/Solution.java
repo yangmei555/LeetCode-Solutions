d441bfc995b9fb2f482a7d4907835709a6368828
yangmei555 <myang3@uchicago.edu>
2018-07-17 23:55:28 -0500

    add some solutions

---
+++ Random/872/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * The rand7() API is already defined in the parent class SolBase.
+ * public int rand7();
+ * @return a random integer in the range 1 to 7
+ */
+class Solution extends SolBase {
+    public int rand10() {
+        int a = 2, b = 2;
+        while ( 7 * a + b > 9) {
+            a = rand7() - 1;
+            b = rand7() - 1;
+        }
+        return 7 * a + b + 1;
+    }
+}
+
+
+/**
+ * The rand7() API is already defined in the parent class SolBase.
+ * public int rand7();
+ * @return a random integer in the range 1 to 7
+ */
+class Solution extends SolBase {
+    public int rand10() {
+        int a = 6, b = 6;
+        while ( 7 * a + b >= 40) {
+            a = rand7() - 1;
+            b = rand7() - 1;
+        }
+        return (7 * a + b) % 10 + 1;
+    }
+}
