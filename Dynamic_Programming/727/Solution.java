72c1bf01746ddfa3c375a2af183a321b849352b0
yangmei555 <myang3@uchicago.edu>
2018-07-21 18:50:38 -0500

    add some solutions

--- Dynamic_Programming/727/Solution.java
+++ Dynamic_Programming/727/Solution.java
@@ -61,3 +61,71 @@ class Solution {
             return S.substring(index+1-min, index+1);
     }
 }
+
+
+class Solution {
+    public String minWindow(String S, String T) {
+        if (S.length() < T.length())
+            return "";
+        if (S.length() == T.length())
+            return S.equals(T) ? S : "";
+        char[] s = S.toCharArray(), t = T.toCharArray();
+        int min = Integer.MAX_VALUE, start = -1;
+        for (int i = 0; i <= s.length-t.length; i++) {
+            if (s[i] == t[0]) {
+                int index = 0, j = i;
+                while (j < s.length) {
+                    if (s[j] == t[index]) {
+                        index++;
+                        if (index == t.length)
+                            break;
+                    }
+                    j++;
+                }
+                if (index == t.length && j-i+1 < min) {
+                    min = j-i+1;
+                    start = i;
+                }
+            }
+        }
+        return start == -1 ? "" : S.substring(start, start+min);
+    }
+}
+
+
+// this is the fastest when running on Leetcode OJ
+class Solution {
+    public String minWindow(String S, String T) {
+        if (S.length() < T.length())
+            return "";
+        if (S.length() == T.length())
+            return S.equals(T) ? S : "";
+        char[] s = S.toCharArray(), t = T.toCharArray();
+        int min = Integer.MAX_VALUE, end = -1, index = 0;
+        int count = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] == t[index]) {
+                index++;
+                if (index == t.length) {
+                    index--;
+                    int start = i;
+                    for ( ; index >= 0; start--) {
+                        count++;
+                        if (s[start] == t[index])
+                            index--;
+                    }
+                    start++;
+                    index++;
+                    if (i-start+1 < min) {
+                        min = i-start+1;
+                        end = i;
+                    }
+                    i = start;
+                    if (i > s.length-t.length)  // remaining S is shorter than T
+                        break;
+                }
+            }
+        }
+        return end == -1 ? "" : S.substring(end+1-min, end+1);
+    }
+}
