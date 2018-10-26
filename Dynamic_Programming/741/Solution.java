1fcc1aea0e202dc5932fc180444710700acdecdb
yangmei555 <myang3@uchicago.edu>
2018-10-26 04:50:31 -0500

    add some solutions

--- Dynamic_Programming/741/Solution.java
+++ Dynamic_Programming/741/Solution.java
@@ -80,7 +80,7 @@ class Solution {
                 int start = Math.max(0, i + j - N + 1);
                 // let ii * N + jj <= i * N + j 
                 for (int ii = start; ii <= i; ii++) {
-                    int jj =  i + j - ii;
+                    int jj = i + j - ii;
                     if (grid[ii][jj] == -1)
                         continue;
                     for (int[] d1 : dir) {
@@ -104,5 +104,146 @@ class Solution {
             }
         }
         return Math.max(0, dp[N-1][N-1][N-1][N-1]);
+    }
+}
+
+
+// notice that, i, j, ii can determine jj ! so only O(N^3) space is enough 
+// also notice that if i + j == ii + jj then (i, j) and (ii, jj) are on the same line !!!  
+class Solution {
+    public int cherryPickup(int[][] grid) {
+        int N = grid.length;
+        int[][] dir = new int[][]{{-1, 0}, {0, -1}};
+        int[][][] dp = new int[N][N][N]; 
+        for (int[][] dp1 : dp) {
+            for (int[] dp2 : dp1) 
+                Arrays.fill(dp2, -1);
+        }
+        dp[0][0][0] = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] == -1)
+                    continue;
+                int start = Math.max(0, i + j - N + 1);
+                for (int ii = start; ii <= i; ii++) {
+                    int jj = i + j - ii;
+                    if (grid[ii][jj] == -1)
+                        continue;
+                    for (int[] d1 : dir) {
+                        if (i+d1[0] >= 0 && j+d1[1] >= 0 && grid[i+d1[0]][j+d1[1]] != -1) {
+                            for (int[] d2 : dir) {
+                                if (ii+d2[0] >= 0 && jj+d2[1] >= 0 && 
+                                                        grid[ii+d2[0]][jj+d2[1]] != -1) 
+                                    dp[i][j][ii] = Math.max(dp[i][j][ii], 
+                                                            dp[i+d1[0]][j+d1[1]][ii+d2[0]]);
+                                
+                            }
+                        }
+                    }
+                    if (dp[i][j][ii] != -1) {
+                        if (grid[i][j] == 1)
+                            dp[i][j][ii]++;
+                        if (ii != i && grid[ii][jj] == 1)
+                            dp[i][j][ii]++;
+                    }
+                }
+            }
+        }
+        return Math.max(0, dp[N-1][N-1][N-1]);
+    }
+}
+
+
+// space complexity O(N^2) 
+class Solution {
+    public int cherryPickup(int[][] grid) {
+        int N = grid.length;
+        int[][] dir = new int[][]{{-1, 0}, {0, -1}};
+        int[][][] dp = new int[2][N][N]; 
+        for (int[][] dp1 : dp) {
+            for (int[] dp2 : dp1)
+                Arrays.fill(dp2, -1);
+        }
+        dp[0][0][0] = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] == -1) 
+                    continue;
+                int start = Math.max(0, i + j - N + 1);
+                for (int ii = start; ii <= i; ii++) {
+                    int jj = i + j - ii;
+                    if (grid[ii][jj] == -1)
+                        continue;
+                    if (i != 0 || j != 0)
+                        dp[0][j][ii] = -1;
+                    for (int[] d1 : dir) {
+                        if (i+d1[0] >= 0 && j+d1[1] >= 0 && grid[i+d1[0]][j+d1[1]] != -1) {
+                            for (int[] d2 : dir) {
+                                if (ii+d2[0] >= 0 && jj+d2[1] >= 0 && 
+                                                        grid[ii+d2[0]][jj+d2[1]] != -1) {
+                                    dp[0][j][ii] = Math.max(dp[0][j][ii], 
+                                d1[0] == -1 ? dp[1][j+d1[1]][ii+d2[0]] : dp[0][j+d1[1]][ii+d2[0]]);
+                                }
+                            }
+                        }
+                    }
+                    if (dp[0][j][ii] != -1) {
+                        if (grid[i][j] == 1)
+                            dp[0][j][ii]++;
+                        if (ii != i && grid[ii][jj] == 1)
+                            dp[0][j][ii]++;
+                    }
+                }
+            }
+            for (int j = 0; j < N; j++)
+                dp[1][j] = dp[0][j].clone();
+        }
+        return Math.max(0, dp[0][N-1][N-1]);
+    }
+}
+
+
+// a more concise O(N^2) space complexity solution 
+class Solution {
+    public int cherryPickup(int[][] grid) {
+        int N = grid.length;
+        int[][] dir = new int[][]{ {0, -1}, {-1, 0}};
+        int[][] dp = new int[N][N]; 
+        for (int[] dp1 : dp) 
+            Arrays.fill(dp1, -1);
+        dp[0][0] = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] == -1) 
+                    continue;
+                int start = Math.max(0, i + j - N + 1);
+                for (int ii = i; ii >= start; ii--) {
+                    int jj = i + j - ii;
+                    if (grid[ii][jj] == -1) 
+                        continue;
+                    int cand = -1;
+                    for (int[] d1 : dir) {
+                        if (i+d1[0] >= 0 && j+d1[1] >= 0 && grid[i+d1[0]][j+d1[1]] != -1) {
+                            for (int[] d2 : dir) {
+                                if (ii+d2[0] >= 0 && jj+d2[1] >= 0 && 
+                                                        grid[ii+d2[0]][jj+d2[1]] != -1) {
+                                    cand = Math.max(cand, dp[j+d1[1]][ii+d2[0]]);
+                                }
+                            }
+                        }
+                    }
+                    if (cand != -1) {
+                        dp[j][ii] = cand;
+                        if (grid[i][j] == 1)
+                            dp[j][ii]++;
+                        if (ii != i && grid[ii][jj] == 1)
+                            dp[j][ii]++;
+                    } else {
+                        dp[j][ii] = i + j == 0 ? grid[i][j] : -1;
+                    }
+                }
+            }
+        }
+        return Math.max(0, dp[N-1][N-1]);
     }
 }
